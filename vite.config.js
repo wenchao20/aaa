7c5b8d1ca3ee05fab6b4b10f824fdcbcae27f34a
wc <wenchao132@gmail.com>
2024-03-12 11:09:04 +0800

    Signed-off-by: wc <wenchao132@gmail.com>

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,20 @@
+/*
+ * @Description: 
+ * @Version: 2.0
+ * @Autor: Seven
+ * @Date: 2024-03-11 23:02:19
+ * @LastEditors: Seven
+ * @LastEditTime: 2024-03-12 11:07:25
+ */
 import vue from "@vitejs/plugin-vue";
 
 /**
  * @type {import('vite').UserConfig}
  */
 export default {
-  plugins: [vue()],
-  build: {
-    base: "./",
-  },
+    plugins: [vue()],
+    build: {
+        base: "./",
+        outDir: 'docs'
+    },
 };
